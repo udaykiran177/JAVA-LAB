acbfdb71b1a8ac800fb47500bb71826b6e457410
udaykiran177 <95631110+udaykiran177@users.noreply.github.com>
2021-12-30 12:46:45 +0530

    week_4_1.java

--- week_4_1.java
+++ week_4_1.java
@@ -1,20 +1,23 @@
 import java.util.*;
 class employee
 { 
-     String name,desig;
+     String name;
+     String desig;
      int id;
      void reademp(){
      Scanner sc=new Scanner(System.in);
      System.out.println("enter the name,id and designation:");
      this.name=sc.nextLine();
-     this.id=sc.nextInt();
      this.desig=sc.nextLine();
+     this.id=sc.nextInt();
+     
      }
 }
 
 class salary extends employee
 {
-      
+      /*String name;
+      String desig;*/
       Scanner sc=new Scanner(System.in);
       void readsalary(){
       System.out.println("enter the bp,hra,da and pf:");
@@ -28,15 +31,16 @@ class salary extends employee
       public void calculate(float bp,float hra,float da,float pf)
       {
           float np=bp+hra+da-pf;
-          this.display(np);
+          this.display(np,name,id,desig);
       }
       
-      public void display(float np)
+      public void display(float np,String name,int id,String desig)
       {
-          
+          /*this.name=name;
+          this.desig=desig;*/
           System.out.println("NAME:"+name);
-          System.out.println("ID:"+this.id);
-          System.out.println("DESIGNATION:"+this.desig);
+          System.out.println("ID:"+id);
+          System.out.println("DESIGNATION:"+desig);
           System.out.println("salary:"+np);
       }
 }
@@ -46,8 +50,8 @@ class week_4_1
       public static void main(String args[])
       {
             salary em=new salary();
-            employee emp=new employee();
-            emp.reademp();
+            //employee emp=new employee();
+            em.reademp();
             em.readsalary();
       }
 }
